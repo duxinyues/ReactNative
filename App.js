23585ad60a537bac82fd4763efa5197dfc22c16e
duxinyues <yongyuan253015@gmail.com>
2022-07-30 22:25:33 +0800

    触摸事件

--- App.js
+++ App.js
@@ -2,31 +2,21 @@
  * @Author: duxinyues yongyuan253015@gmail.com
  * @Date: 2022-07-26 19:54:23
  * @LastEditors: duxinyues yongyuan253015@gmail.com
- * @LastEditTime: 2022-07-28 23:38:46
+ * @LastEditTime: 2022-07-30 21:06:33
  * @FilePath: \AwesomeProject\App.js
  * @Description:
  * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
  */
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import Hello from './src/pages/hello';
+import {View} from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.flexBox}>
-      <Text style={styles.text}>测试组件</Text>
-      <Text>98789</Text>
+    <View>
+      <Hello />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  flexBox: {
-    flex: 3,
-  },
-  text: {
-    color: 'red',
-    fontSize: 30,
-  },
-});
 
 export default App;
---
+++ src/pages/hello.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-alert */
+/* eslint-disable react-native/no-inline-styles */
+/*
+ * @Author: duxinyues yongyuan253015@gmail.com
+ * @Date: 2022-07-30 13:57:19
+ * @LastEditors: duxinyues yongyuan253015@gmail.com
+ * @LastEditTime: 2022-07-30 21:52:40
+ * @FilePath: \AwesomeProject\src\pages\hello.js
+ * @Description: 开始页面
+ * Copyright (c) 2022 by duxinyues email: yongyuan253015@gmail.com, All Rights Reserved.
+ */
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight,
+} from 'react-native';
+
+const Hello = () => {
+  const onPressIn = () => {
+    console.log('按住');
+  };
+  const onPressOut = () => {
+    console.log('松开');
+  };
+  const onLongPress = () => {
+    console.log('长按');
+  };
+  const onPress = () => {
+    console.log('触摸');
+    alert('消息弹窗');
+  };
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.flexBox}>
+        <Image
+          source={{
+            uri: 'https://profile-avatar.csdnimg.cn/67927e8210de430e86d119a130c1dca2_xuelian3015.jpg!1',
+          }}
+          style={styles.image}
+        />
+      </View>
+      <TextInput style={styles.input} defaultValue="这是一个输入框" />
+      <TouchableHighlight
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}
+        onLongPress={onLongPress}
+        onPress={onPress}>
+        <View>
+          <Text style={styles.button}>按钮</Text>
+        </View>
+      </TouchableHighlight>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+  },
+  flexBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  input: {
+    width: 300,
+    height: 35,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 20,
+  },
+  button: {
+    marginTop: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 100,
+    borderColor: 'blue',
+    borderWidth: 1,
+    textAlign: 'center',
+  },
+});
+export default Hello;
